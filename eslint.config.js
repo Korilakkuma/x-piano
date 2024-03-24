f086995cc89020ff957f86a616eb6cdee415488e
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-03-24 23:39:49 +0900

    chore: Linter and Formatter config

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+module.exports = [
+  {
+    'files': ['src/**/*.js', 'jest.config.js', 'webpack.config.js'],
+    'languageOptions': {
+      'globals': {
+        'XSound': 'readonly',
+        'X': 'readonly'
+      }
+    },
+    'rules': {
+      'default-param-last': 'off',
+      'indent': [
+        'error',
+        2,
+        {
+          'ignoredNodes': ['TemplateLiteral'],
+          'SwitchCase': 1
+        }
+      ],
+      'key-spacing': 'off',
+      'jsx-a11y/no-onchange': 'off',
+      'no-case-declarations': 'off',
+      'no-console': 'warn',
+      'no-constant-condition': 'off',
+      'no-else-return': 'error',
+      'no-multi-spaces': 'off',
+      'no-unneeded-ternary': 'off',
+      'no-unused-vars': ['off', { 'vars': 'all', 'args': 'after-used' }],
+      'no-use-before-define': 'off',
+      'no-var': 'warn',
+      'prefer-promise-reject-errors': 'off',
+      'quotes': ['error', 'single'],
+      'quote-props': 'off',
+      'radix': 'warn',
+      'semi': ['error', 'always'],
+      'space-before-function-paren': 'off',
+      'template-curly-spacing': 'off'
+    }
+  }
+];
